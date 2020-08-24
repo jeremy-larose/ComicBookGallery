f789645ff5e5e468e67b9a48dfa1a38eebf9ad1e
Jeremy LaRose <53305810+jeremy-larose@users.noreply.github.com>
2020-08-24 14:14:06 -0400

    Added detail action method

--- Comic-Book-Gallery/Controllers/ComicBooksController.cs
+++ Comic-Book-Gallery/Controllers/ComicBooksController.cs
@@ -4,6 +4,9 @@ namespace Comic_Book_Gallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        
+        public string Detail()
+        {
+            return "Hello from the comic books controller!";
+        }
     }
 }

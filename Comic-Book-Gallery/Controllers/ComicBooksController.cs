acf8c09d3c355320cb6c8763bffab083ae77bb91
Jeremy LaRose <53305810+jeremy-larose@users.noreply.github.com>
2020-08-31 07:50:31 -0400

    Updated the ComicBook Detail View layout.

--- Comic-Book-Gallery/Controllers/ComicBooksController.cs
+++ Comic-Book-Gallery/Controllers/ComicBooksController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Web.Mvc;
+using Comic_Book_Gallery.Models;
 
 namespace Comic_Book_Gallery.Controllers
 {
@@ -7,19 +8,21 @@ namespace Comic_Book_Gallery.Controllers
     {
         public ActionResult Detail()
         {
-            
-            ViewBag.SeriesTitle = "The Amazing Spider-Man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
-            {
-                "Scripts: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+            var comicBook = new ComicBook()
+            {            
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name = "Dan Slott", Role = "Scripts"},
+                    new Artist() {Name = "Humberto Ramos", Role = "Pencils"},
+                    new Artist() {Name = "Victor Olazaba", Role = "Inks"},
+                    new Artist() {Name = "Edgar Delgado", Role = "Colors"},
+                    new Artist() {Name = "Chris Eliopoulos", Role = "Letters"}
+                }
             };
-            return View();
+            return View(comicBook);
         }
     }
 }
